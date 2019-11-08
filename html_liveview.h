600110e6a0e85c5e75d4ab612d0524bd77008207
photocromax <56951523+photocromax@users.noreply.github.com>
2019-11-08 10:39:32 +0100

    Update html_liveview.h

--- html_liveview.h
+++ html_liveview.h
@@ -1,6 +1,6 @@
 /*
- * LiveView html
- */
+   LiveView html
+*/
 
 // Live View
 const char PAGE_liveView[] PROGMEM = R"=====(<!DOCTYPE html>
@@ -8,22 +8,32 @@ const char PAGE_liveView[] PROGMEM = R"=====(<!DOCTYPE html>
 <meta http-equiv="refresh" content="3600">
 <style>html, body {margin: 0;padding: 0;justify-content: center;}canvas {display: block;}</style>
 <title>Live View</title>
-<body>
-<canvas id="liveCanvas"  width="304" height="16" style="width: 304px; height: 16px; position: absolute; top: 0px; left: 0px; right: 0px;  margin: auto;"></canvas>
+<body style="background-color:#333333;" >
+<canvas id="liveCanvas"  width="5" height="5" style="width: 5px; height: 5px; position: absolute; top: 0px; left: 0px; right: 0px;  margin: auto;"></canvas>
 <script>
-var namespace = {};
-url = "/json/live";  // live strip state json format 
-lightSize = 16; // light box size
-numLights = 1; 
-prev_leds = ([0,"#000000",-1]);  //previous strip state
-namespace.strip = {"leds":[0,"#ffffff",0]}; //arbitrary strip state
-var canvas = document.getElementById("liveCanvas");  
 
-function loadJSON(url,cbk,cbkError){
+
+
+var windowWidth = 500;
+var windowHeight = 500;
+var lightSize = 16; // light box size
+var lines=0;
+var multipartSize=%MPS%;  //refer to wled21_liveview multipartSize global variable. (it has to be the same value)
+var lightPerLines=1;
+var numLights = 0; 
+var canvas ;  
+var ctx ;
+var m=-1;
+var mi=-1;
+var mpi=0;
+
+prev_leds = (["#000000"]);  //previous strip state
+
+function pcm_loadJSON(cbk,cbkError){
     var httpReq = new XMLHttpRequest(); 
     httpReq.overrideMimeType("application/json");
     httpReq.addEventListener("error", cbkError, false);
-    httpReq.open("GET",url,true);
+    httpReq.open("GET","/json/live",true);
     httpReq.responseType="json";
     httpReq.onload=function(e) {
       var json= httpReq.response;
@@ -35,52 +45,118 @@ function loadJSON(url,cbk,cbkError){
     httpReq.send();   
 }
 
-loadJSON(url,drawLights,errorDrawLights); //get actual strip state
 
-function resizeCanvas(w,h,noRedraw){
- canvas.width=w;
- canvas.height=h;
- canvas.style.width=w+"px";
- canvas.style.height=h+"px";
+function pcm_resizeCanvas(noRedraw){
+  if (!noRedraw) {
+   pcm_clearCanvas(canvas);
+  }
+  lightsPerLine = Math.floor(windowWidth / lightSize ); 
+  lines = Math.ceil(numLights / lightsPerLine)
+  if (lines == 0) {
+    lines=1;
+  }  
+  w = lightsPerLine*lightSize+1;
+  h = lines*lightSize+1;
+  canvas.width=w;
+  canvas.height=h;
+  canvas.style.width=w+"px";
+  canvas.style.height=h+"px";
 }
 
-function renderLight(light, col) {
-  var ctx = canvas.getContext("2d");
-  ctx.fillStyle = col;
-  ctx.fillRect(light * lightSize, 0, lightSize, lightSize); //create the square representing the light (left, top, width, height)
-  ctx.strokeStyle = "color";
-  ctx.lineWidth = 1;
-  ctx.strokeRect(light * lightSize, 0, lightSize, lightSize); //create the stroke (left, top, width, height)
+
+function pcm_clearCanvas(cnv) {
+ //var ctx = canvas.getContext("2d");  
+ ctx.clearRect(0,0,cnv.width,cnv.height);
+  for (i = 0; i < numLights ; i++) {
+       renderLight(i, "",true);
+  }
+}
+
+function isOdd(n) {
+  if (Math.floor(n/2)*2 != n) return true;
+  return false;
+}
+
+function renderLight(light, col, stroke) {
+  line=Math.floor(light / lightsPerLine);
+    if (isOdd(line)){
+      reverse=lightsPerLine*lightSize-lightSize;
+      drawDirection=-1;
+    } else {
+    reverse=0;
+    drawDirection=1;
+  }
+  x = reverse+drawDirection*((light %% lightsPerLine) * lightSize+0.5*drawDirection);
+  y = line * lightSize+0.5;
+  //var ctx = canvas.getContext("2d");
+  if (!stroke){
+    ctx.fillStyle = col; 
+    ctx.fillRect(x+0.5, y+0.5, lightSize-1, lightSize-1); //create the square representing the light (left, top, width, height)
+  } else {
+    ctx.strokeStyle = "#333333";
+    ctx.lineWidth = 1;
+    ctx.strokeRect(x, y, lightSize, lightSize); //create the stroke (left, top, width, height)
+  }
 }
 
 function errorDrawLights(err){
-  window.location.reload();  //reload the page
-  console.log("frame reloaded");
+  pcm_loadJSON(drawLights,errorDrawLights); //get actual strip state AGAIN
+  //console.log("frame reloaded");
 }
 
 function drawLights(strip) {
-  if( prev_leds==strip.leds) {
+  try {
+    if (strip === null) throw "empty json";
+  }
+  catch(err) {
+    delete strip;  // local variable to the GC
+    errorDrawLights();
+    //console.log(err);
+    return "";
+  }
+  if ( numLights != strip.lc) {
+      numLights = strip.lc;  
+      pcm_resizeCanvas(false);
+  }
+  else if( prev_leds==strip.leds  && m==strip.m && mpi==strip.mpi) {
     mi=0; //millis since last frame
   } else {
-    if ( numLights != strip.leds.length-2) {
-      numLights = strip.leds.length-2; 
-      resizeCanvas(numLights * lightSize, lightSize, true);
+    m=strip.m;  // effect mode index
+    mpi=strip.mpi //multipartIndex
+    mi=strip.mi; // millis since last frame
+    prev_leds=strip.leds; //update previous strip state with actual state
+    pl=strip.leds.length; // part length 
+        for (i = 0; i < pl; i++) {
+       renderLight(i+mpi*multipartSize, strip.leds[i],false);
     }
-    for (i = 0; i < numLights+2; i++) {
-      if(i==0) { 
-        m=strip.leds[i];  // get effect mode index
-      } else if (i==numLights+1){
-          mi=strip.leds[i]; //millis since last frame
-      } else {
-      renderLight(i-1, strip.leds[i]);
-      }
-    }
-    prev_leds=strip.leds; //update previpus strip state with actual state
-    delete strip;  // local variable to the GC
-    delete namespace.strip; // global variable to the GC
   }
-  loadJSON(url,drawLights,errorDrawLights);  //request new  strip state
+  delete strip;  // local variable to the GC
+  pcm_loadJSON(drawLights,errorDrawLights);  //request new  strip state
 }
+
+function getWindowSize(){
+  windowWidth = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth)-30;
+  windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+}
+
+function windowResize(){
+   getWindowSize();
+   pcm_resizeCanvas(false);  
+ }  
+
+function pcm_setup() {
+  canvas = document.getElementById("liveCanvas");  
+  ctx = canvas.getContext("2d");
+  window.onresize = windowResize;
+  getWindowSize();
+  pcm_resizeCanvas(false);
+  pcm_loadJSON(drawLights,errorDrawLights); //get actual strip state
+}
+
+pcm_setup();
 </script>
 </body>
 </html>)=====";
+
+
+
