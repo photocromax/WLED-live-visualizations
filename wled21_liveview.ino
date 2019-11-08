55401f5cd82c47cb95f4d24026ebdafb49752c68
photocromax <56951523+photocromax@users.noreply.github.com>
2019-11-08 10:41:24 +0100

    Update wled21_liveview.ino

--- wled21_liveview.ino
+++ wled21_liveview.ino
@@ -2,22 +2,27 @@
    Live View
 */
 
-uint8_t fpsMaxLimit = 200;
-uint8_t delayMaxLimit = (uint8_t)(1000.0 / fpsMaxLimit);
+uint16_t fpsMaxLimit = 60;
+uint16_t delayMaxLimit = 0;//(uint16_t)(1000.0 / ((float)fpsMaxLimit*ledCount / multipartSize));
+
 String prev_bufVisualization = "default";
 uint32_t prev_millis = 0;
+uint32_t multipartIndex = 0;
+
+
+
 
 
 String getLiveLights() {
   String bufVisualization = "";
-  if ((millis() > prev_millis + delayMaxLimit)  ||  (prev_bufVisualization == "default")) {  // if false serve the old buffer
-    for (uint16_t i = 0; i < ledCount; i++) {
-      if (i == 0) {  // index 0 will contains the effect number
-        bufVisualization = "{\"leds\":[" + String(strip.getMode()) + ",";
+  if ((millis() > prev_millis + delayMaxLimit)  ||  (prev_bufVisualization == "default")) {  // if false serve empty buffer
+    for (uint16_t i = 0; i < multipartSize && i + multipartIndex * multipartSize < ledCount; i++) {
+      if (i == 0) {  // add extra fields
+        bufVisualization = "{\"m\":" + String(strip.getMode()) + ", \"lc\":" + ledCount + ", \"leds\":[";
       } else {
         bufVisualization += ",";
       }
-      CRGB fastled_col = strip.col_to_crgb(strip.getPixelColor(i));
+      CRGB fastled_col = strip.col_to_crgb(strip.getPixelColor(i + multipartIndex * multipartSize));
       String col = "";
       bufVisualization += "\"";
       //bufVisualization += strip.getPixelColor(i);
@@ -31,23 +36,29 @@ String getLiveLights() {
       bufVisualization += col.substring(col.length() - 2);
       bufVisualization += "\"";
     }
-    if (bufVisualization != prev_bufVisualization ) {  //if there is a new strip state
+    if (bufVisualization != prev_bufVisualization || ledCount > multipartSize ) { //if there is a new strip state or strip is longer than multipartiSize
       prev_bufVisualization = bufVisualization;  //update the previous buffer
+      if (ledCount > multipartSize ) {
+        multipartIndex++;
+        if (multipartIndex * multipartSize >= ledCount) {
+          multipartIndex = 0;
+        }
+      }
+      // add millis since last update (maybe for future optimizations)
+      uint32_t m = millis();
+      bufVisualization += "], \"mpi\":" + String(multipartIndex) + "";
+      bufVisualization += ", \"mi\":" + String(m - prev_millis);
+      bufVisualization += "}";
+      prev_millis = m;
+      return bufVisualization;
     }
-  } else {
-    bufVisualization = prev_bufVisualization; // serve the old buffer
   }
-
-  // add millis since last update (maybe for future optimizations)
-  uint32_t m = millis();
-  bufVisualization += "," + String(m - prev_millis);
-  prev_millis = m;
-
-  bufVisualization += "]}";
-  return bufVisualization;
+  return String("");
 }
 
-void serveLiveView(AsyncWebServerRequest* request)
+
+
+void serveLiveView(AsyncWebServerRequest * request)
 {
-  request->send_P(200, "text/html", PAGE_liveView);
+  request->send_P(200, "text/html", PAGE_liveView, settingsProcessor);
 }
